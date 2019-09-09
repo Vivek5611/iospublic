7335d66ff72693b82ff282b8f7beabcd22c02edb
Vivek5611 <53434403+Vivek5611@users.noreply.github.com>
2019-09-09 13:04:03 +0530

    Add files via upload

--- ViewController.swift
+++ ViewController.swift
@@ -1,107 +1,50 @@
-
 import UIKit
+import AVFoundation
+class ViewController: UIViewController ,AVAudioPlayerDelegate{
+    
+    var audioPlayer = AVAudioPlayer()
 
-class ViewController: UIViewController,UITextFieldDelegate,UITableViewDataSource, UITableViewDelegate {
-   
-    @IBOutlet var tableView: UITableView!
-   @IBOutlet weak var txtFldName: UITextField!
-    @IBOutlet var txtFldEmail: UITextField!
-    @IBOutlet var txtFldroll: UITextField!
-    @IBOutlet var txtFldSection: UITextField!
+    @IBAction func playBtn(_ sender: Any) {
+        audioPlayer.play()
+        }
     
-    @IBOutlet var updateBtn: UIButton!
+    @IBAction func pauseBtn(_ sender: Any) {
+        audioPlayer.pause()
+        }
     
-    let nameArr = NSMutableArray()
-    var locArr = Int()
-    
+    @IBAction func restartbtn(_ sender: Any) {
+        audioPlayer.currentTime = 0
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-     
-        tableView.delegate = self
-        tableView.dataSource = self
-
-    }
-    @IBAction func addActionBtn(_ sender: Any) {
-         let dict = NSMutableDictionary()
-        dict.setValue(txtFldName.text, forKey: "name")
-        dict.setValue(txtFldEmail.text, forKey: "Email")
-        dict.setValue(txtFldroll.text, forKey: "roll")
-        dict.setValue(txtFldSection.text, forKey: "section")
+        let url = URL.init(fileURLWithPath: Bundle.main.path(
+            forResource: "music",
+            ofType: "mp3")!)
         
-       nameArr.add(dict)
-        txtFldName.text = ""
-        txtFldEmail.text = ""
-        txtFldroll.text = ""
-        txtFldSection.text = ""
-       tableView.reloadData()
-    }
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        //            print(nsMute[indexPath.row]) as! <#type#>
-        let cell = tableView.dequeueReusableCell(withIdentifier: "cell") as! cell
-        let dictCell = nameArr[indexPath.row] as! NSMutableDictionary
-        cell.nameLbl.text = dictCell["name"] as? String
-        cell.RollLbl.text = dictCell["Email"] as? String
-        cell.EmailLbl.text = dictCell["roll"] as? String
-        cell.SectionLbl.text = dictCell["section"] as? String
-        return cell
-    }
-    @IBAction func updateBtn(_ sender: Any) {
-        let indexpath = IndexPath(row: locArr, section: 0)
-        let cellData = nameArr[indexpath.row] as! NSMutableDictionary
-        cellData["name"] = txtFldName.text
-        cellData["Email"] = txtFldEmail.text
-        cellData["roll"] = txtFldroll.text
-        cellData["section"] = txtFldSection.text
-        txtFldName.text = ""
-        txtFldEmail.text = ""
-        txtFldroll.text = ""
-        txtFldSection.text = ""
-        tableView.reloadData()
-        
-    }
-    
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return nameArr.count
-    }
-    
-    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 200
-    }
-    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete{
-            self.nameArr.removeObject(at: indexPath.row)
-            self.tableView.deleteRows(at: [indexPath], with: .automatic)
-           
-            }
+        do {
+            try audioPlayer = AVAudioPlayer(contentsOf: url)
+            audioPlayer.delegate = self
+            audioPlayer.prepareToPlay()
+        } catch let error as NSError {
+            print("audioPlayer error \(error.localizedDescription)")
         }
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        let dictCell = nameArr[indexPath.row] as! NSMutableDictionary
-        txtFldName.text = dictCell["name"] as? String
+        func audioPlayerDidFinishPlaying(_ player: AVAudioPlayer, successfully
+            flag: Bool) {
+        }
         
-         txtFldEmail.text = dictCell["Email"] as? String
-         txtFldroll.text = dictCell["roll"] as? String
-         txtFldSection.text = dictCell["section"] as? String
-        tableView.reloadData()
-    }
-  
-    @IBAction func homeBtn(_ sender: Any) {
-        let detailVc = storyboard?.instantiateViewController(withIdentifier: "ViewController2") as! ViewController2
-          let indexpath = IndexPath(row: locArr, section: 0)
-        let dictCell = nameArr[indexpath.row] as! NSMutableDictionary
+        func audioPlayerDecodeErrorDidOccur(_ player: AVAudioPlayer,
+                                            error: Error?) {
+        }
         
-            detailVc.name1 = dictCell["name"] as! String
-        detailVc.email1 = dictCell["Email"] as! String
-        detailVc.roll1 = dictCell["roll"] as! String
-        detailVc.section1 = dictCell["section"] as! String
+        func audioPlayerBeginInterruption(_ player: AVAudioPlayer) {
+        }
         
-        
-        navigationController?.pushViewController(detailVc, animated: true)
-        
+        func audioPlayerEndInterruption(player: AVAudioPlayer) {
         }
     }
 
-
-
+    
+}
+    
 
 
